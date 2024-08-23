2d044d48a7386ead94c4d470dc827f6dff34737d
davidfeldner <davidfeldner12@gmail.com>
2024-08-23 21:22:05 +0200

    Hyprland monitor and nvidia options

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -2,5 +2,10 @@
 
 {
   imports = [ ../../home/general.nix ];
-  hyprland.extraMonitorSettings = [ ];
+  hyprland.extraMonitorSettings = [
+    "DVI-D-1, 1440x900,      4480x0, 1"
+    "DP-2,    2560x1440@144, 1920x0, 1"
+    "HDMI-A-2, disable"
+  ];
+  hyprland.nvidia = true;
 }
--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -2,4 +2,5 @@
 
 {
   imports = [ ../../home/general.nix ];
+  hyprland.extraMonitorSettings = [ "eDP-1,3072x1920@120,0x0,2" ];
 }
