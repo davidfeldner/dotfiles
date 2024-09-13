5dc59aeef4953c47c0bb58b95040ab2c7691911d
davidfeldner <davidfeldner12@gmail.com>
2024-09-13 12:27:35 +0200

    Better tmux conf with yank and start folder fix

--- home/tmux.nix
+++ home/tmux.nix
@@ -4,9 +4,20 @@
   programs.tmux = {
     enable = true;
     mouse = true;
-    plugins = with pkgs.tmuxPlugins; [ yank ];
+    plugins = with pkgs.tmuxPlugins; [
+      yank
+      dracula
+    ];
     extraConfig = ''
       set -g @yank_action 'copy-pipe'
+      set -g @yank_with_mouse off
+      unbind -T copy-mode-vi MouseDragEnd1Pane
+      unbind -T copy-mode MouseDragEnd1Pane
+
+      # New window path
+      bind  c  new-window      -c "#{pane_current_path}"
+      bind  %  split-window -h -c "#{pane_current_path}"
+      bind '"' split-window -v -c "#{pane_current_path}"
     '';
   };
 }
