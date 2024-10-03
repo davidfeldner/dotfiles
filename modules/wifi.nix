d863d429af8a03140e63af5f5e9922ffaf8ffdb0
davidfeldner <davidfeldner12@gmail.com>
2024-10-03 18:09:41 +0200

    Fix patch path

--- modules/wifi.nix
+++ modules/wifi.nix
@@ -7,12 +7,12 @@
     wifi.powersave = false;
   };
 
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # https://kisonecat.com/blog/eduroam-openssl-wpa-supplicant/
   nixpkgs.config.packageOverrides = pkgs: {
     wpa_supplicant = pkgs.wpa_supplicant.overrideAttrs (attrs: {
-      patches = attrs.patches ++ [ ./overrides/eduroam.patch ];
+      patches = attrs.patches ++ [ ../overrides/eduroam.patch ];
     });
   };
 }
