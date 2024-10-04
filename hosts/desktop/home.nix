51bebdeb812a594a0e1f122e3e536e2be8c1c256
davidfeldner <davidfeldner12@gmail.com>
2024-10-05 01:30:39 +0200

    Update desktop monitor config

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -4,8 +4,8 @@
   imports = [ ../../home/general.nix ];
   hyprland.extraMonitorSettings = [
     "DVI-D-1, 1440x900,      4480x0, 1"
-    "DP-2,    2560x1440@144, 1920x0, 1"
-    "HDMI-A-2, disable"
+    "DP-3,    2560x1440@144, 1920x0, 1"
+    "HDMI-A-5, disable"
   ];
   hyprland.nvidia = true;
   hyprland.extraWorkspaceSettings = [
