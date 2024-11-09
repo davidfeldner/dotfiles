eaa97b5d1ce01d429d02e57f88349653f8f7ac05
davidfeldner <davidfeldner12@gmail.com>
2024-11-09 19:12:18 +0100

    Add tv on/off aliases

--- home/zsh.nix
+++ home/zsh.nix
@@ -21,6 +21,8 @@ in
         nxtest = "sudo nixos-rebuild test --flake ~/nixos/";
         icat = "kitten icat";
         lofi = "mpv --no-video 'https://www.youtube.com/watch?v=jfKfPfyJRdk'";
+        tvOff = " hyprctl keyword monitor $(hyprctl monitors all | grep 'HDMI'| awk '{print $2}' | head -n 1), disable";
+        tvOn = " hyprctl keyword monitor $(hyprctl monitors all | grep 'HDMI'| awk '{print $2}' | head -n 1), 3840x2160@60, 0x0, 2";
       };
 
       shellAliases.windows = lib.mkIf cfg.dualboot "sudo grub-reboot 2 && reboot";
