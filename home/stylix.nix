7f461a51ce4049e7bb2ac5ff0b872d08af078f98
davidfeldner <davidfeldner12@gmail.com>
2024-11-18 16:59:59 +0100

    Stylix allow waybar, ignore kitty

--- home/stylix.nix
+++ home/stylix.nix
@@ -3,9 +3,10 @@
   stylix.enable = true;
   stylix.image = ./wall.jpg;
   stylix.polarity = "dark";
-  stylix.targets.waybar.enable = false;
-  stylix.targets.waybar.enableCenterBackColors = false;
-  stylix.targets.waybar.enableLeftBackColors = false;
-  stylix.targets.waybar.enableRightBackColors = false;
+  # stylix.targets.waybar.enable = false;
+  # stylix.targets.waybar.enableCenterBackColors = false;
+  # stylix.targets.waybar.enableLeftBackColors = false;
+  # stylix.targets.waybar.enableRightBackColors = false;
   stylix.targets.neovim.enable = false;
+  stylix.targets.kitty.enable = false;
 }
