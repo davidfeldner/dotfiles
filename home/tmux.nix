ef348347bb29c1604e3db4c790416fe961ffb271
davidfeldner <davidfeldner12@gmail.com>
2024-11-18 17:04:58 +0100

    Tmux at top, with catppuccin theme

--- home/tmux.nix
+++ home/tmux.nix
@@ -6,9 +6,22 @@
     mouse = true;
     plugins = with pkgs.tmuxPlugins; [
       yank
-      dracula
+      catppuccin
     ];
+
     extraConfig = ''
+      set-option -g status-position top 
+
+      set -g @catppuccin_window_default_text "#W"
+      set -g @catppuccin_window_current_text "#W"
+
+      set -g @ctp_bg "#ff0000"
+      set -g @ctp_surface_1 "#00ff00"
+      set -g @ctp_fg "#0000ff"
+      set -g @ctp_mauve "#00ffff"
+      set -g @ctp_crust "#ffff00"
+
+
       set -g @yank_action 'copy-pipe'
       set -g @yank_with_mouse off
       unbind -T copy-mode-vi MouseDragEnd1Pane
