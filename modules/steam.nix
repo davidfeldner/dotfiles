fc8c949e4732798963d74c31805b54b8fc8085b0
davidfeldner <davidfeldner12@gmail.com>
2025-01-18 18:30:22 +0100

    Use steam gamescope

--- modules/steam.nix
+++ modules/steam.nix
@@ -5,5 +5,10 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    gamescopeSession.enable = true;
+  };
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
   };
 }
