7950b4c86e13a806d25018cf091eb4e94db29828
davidfeldner <davidfeldner12@gmail.com>
2025-01-18 18:35:00 +0100

    Use vrr on desktop

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -6,7 +6,7 @@
   ];
   hyprland.extraMonitorSettings = [
     "DVI-D-1, 1440x900,      4480x0, 1"
-    "DP-2,    2560x1440@144, 1920x0, 1"
+    "DP-2,    2560x1440@144, 1920x0, 1, vrr, 1"
     "DP-3,    2560x1440@144, 1920x0, 1"
     "HDMI-A-2, disable"
     "HDMI-A-5, disable"
