e87b2ca4fc73caed56b56575c992d7b73b0bd03a
davidfeldner <davidfeldner12@gmail.com>
2025-02-27 23:20:01 +0100

    Remove old ssh hosts & add new ones

--- home/ssh.nix
+++ home/ssh.nix
@@ -4,23 +4,18 @@
   programs.ssh = {
     enable = true;
     matchBlocks = {
-      "cos" = {
-        hostname = "cos.itu.dk";
-        user = "dafe";
-        identityFile = "/home/david/Nextcloud/School/OSC/OSC";
+      "under" = {
+        hostname = "109.106.244.203";
+        user = "root";
+      };
+      "sim" = {
+        hostname = "206.81.20.113";
+        user = "root";
+        identityFile = "/home/david/6sem/devops/devops";
       };
       "pi" = {
         hostname = "192.168.1.44";
         user = "pi";
-      };
-      "secJump" = {
-        hostname = "130.226.143.130";
-        user = "pensim";
-      };
-      "sec" = {
-        hostname = "10.0.1.23";
-        user = "student";
-        proxyJump = "secJump";
       };
     };
   };
