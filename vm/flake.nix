39763ea26b931b4b6950e3677ad9716221039cd9
davidfeldner <davidfeldner12@gmail.com>
2025-03-09 13:27:22 +0100

    Update vm config

--- vm/flake.nix
+++ vm/flake.nix
@@ -9,28 +9,38 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nur.url = "github:nix-community/NUR";
   };
-  outputs = { self, nixpkgs, nixos-generators, nur, home-manager, ... }: {
-    packages.x86_64-linux = {
-      empty = nixos-generators.nixosGenerate {
-        system = "x86_64-linux";
-        modules = [
-          # NUR setup and overlay
-          nur.nixosModules.nur
-          { nixpkgs.overlays = [ nur.overlay ]; }
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-generators,
+      nur,
+      home-manager,
+      ...
+    }:
+    {
+      packages.x86_64-linux = {
+        empty = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+          modules = [
+            # NUR setup and overlay
+            nur.modules.nixos.default
+            { nixpkgs.overlays = [ nur.overlay ]; }
 
-          ./vm.nix
-          ./modules/vpn.nix
-          home-manager.nixosModules.home-manager {
+            ./vm.nix
+            ./modules/vpn.nix
+            home-manager.nixosModules.home-manager
+            {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.alice = import ./home.nix;
-          }
-        ];
-        format = "qcow";
-        specialArgs = {
+            }
+          ];
+          format = "qcow";
+          specialArgs = {
             diskSize = 20 * 1024;
+          };
         };
       };
     };
-  };
 }
--- vm/gen.sh
+++ vm/gen.sh
@@ -2,11 +2,14 @@ echo "Removing Image"
 rm nixos.qcow2
 
 echo "Building new image"
-nix build .#empty
+nix build path:.#empty
 
 echo "Copying image"
 mkdir -p ~/vms
 cp ./result/nixos.qcow2 ~/vms
 
 echo "Fixing permissions"
-chmod 666 nixos.qcow2
+chmod 666 ~/vms/nixos.qcow2
+
+echo "Removing Original image"
+rm -rf ./result
