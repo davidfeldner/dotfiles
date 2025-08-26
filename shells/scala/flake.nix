0400a5ab36e6f8d19c53dd564167733e512277ef
davidfeldner <davidfeldner12@gmail.com>
2025-08-26 11:50:43 +0200

    Add scala shell

---
+++ shells/scala/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Scala dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          scala-cli
+          coursier
+          metals
+        ];
+        shellHook = '''';
+      };
+    };
+}
