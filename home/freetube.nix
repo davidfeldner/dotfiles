62ce4093ed9937d1940d1a6ff33d094b44d67b93
davidfeldner <davidfeldner12@gmail.com>
2025-08-26 11:17:08 +0200

    Update Freetube

--- home/freetube.nix
+++ home/freetube.nix
@@ -1,15 +1,16 @@
 {
   pkgs,
+  lib,
   ...
 }:
 let
   freetube = pkgs.freetube.overrideAttrs (oldAttrs: rec {
-    version = "0.23.7";
+    version = "0.23.8";
     src = pkgs.fetchFromGitHub {
       owner = "FreeTubeApp";
       repo = "FreeTube";
-      tag = "v0.23.7-beta";
-      hash = "sha256-252d80xCWBZnPHnRESxRqYzT40Gu/LLBbzXr2nIJW/I=";
+      tag = "v0.23.8-beta";
+      hash = "sha256-CHp/6/E/v6UdSe3xoB66Ot24WuZDPdmNyUG1w2w3bX0=";
     };
     yarnHash = "sha256-ia5wLRt3Hmo4/dsB1/rhGWGJ7LMnVR9ju9lSlQZDTTg=";
     yarnOfflineCache = pkgs.fetchYarnDeps {
@@ -19,5 +20,5 @@ let
   });
 in
 {
-  home.packages = [ pkgs.freetube ];
+  home.packages = [ freetube ];
 }
