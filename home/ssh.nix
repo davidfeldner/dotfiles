40166220b06ee1b564ec8ac382607961c4451772
davidfeldner <davidfeldner12@gmail.com>
2025-09-03 18:22:55 +0200

    Manually specify ssh default config, as suggested after update

--- home/ssh.nix
+++ home/ssh.nix
@@ -3,7 +3,22 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks = {
+      "*" = {
+        forwardAgent = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        compression = false;
+        addKeysToAgent = "false";
+
+        controlMaster = "auto";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "10m";
+
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+      };
       "under" = {
         hostname = "109.106.244.203";
         user = "itunderground";
