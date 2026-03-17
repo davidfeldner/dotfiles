1c7d6d00bb0ab68491053ccf18bd74dc3a2fc8d7
davidfeldner <davidfeldner12@gmail.com>
2026-03-18 00:24:12 +0100

    Update desktop nfs settings

--- modules/nixosModules/nfs-desktop.nix
+++ modules/nixosModules/nfs-desktop.nix
@@ -1,17 +1,8 @@
 { ... }:
 {
   flake.nixosModules.nfs-desktop = {
-    fileSystems."/mnt/cache" = {
-      device = "192.168.1.211:/cache";
-      fsType = "nfs";
-      options = [
-        "x-systemd.automount"
-        "noauto"
-        "x-systemd.idle-timeout=600"
-      ];
-    };
-    fileSystems."/mnt/media" = {
-      device = "192.168.1.211:/media";
+    fileSystems."/mnt/storage" = {
+      device = "192.168.1.211:/storage";
       fsType = "nfs";
       options = [
         "x-systemd.automount"
