70583d0e08f05c374deed7a826d89e0fd6aa4ef7
davidfeldner <davidfeldner12@gmail.com>
2026-03-23 07:54:12 +0100

    Update desktop monitor config

--- modules/hosts/desktop/home.nix
+++ modules/hosts/desktop/home.nix
@@ -7,10 +7,8 @@
         self.modules.homeManager.general
       ];
       hyprland.extraMonitorSettings = [
-        "DVI-D-1, 1440x900,      4480x0, 1"
+        "DP-1, 1440x900,      4480x0, 1"
         "DP-2,    2560x1440@144, 1920x0, 1, vrr, 1"
-        "DP-3,    2560x1440@144, 1920x0, 1, vrr, 1"
-        "DP-4,    2560x1440@144, 1920x0, 1, vrr, 1"
         "HDMI-A-1, disable"
         "HDMI-A-2, disable"
         "HDMI-A-3, disable"
@@ -21,8 +19,8 @@
       ];
       hyprland.extraWorkspaceSettings = [
         "1,monitor:DP-2,default:true"
-        "8,monitor:DVI-D-1,default:true"
-        "9,monitor:DVI-D-1"
+        "8,monitor:DP-1,default:true"
+        "9,monitor:DP-1"
       ];
       home.packages = with pkgs; [
         grub2
