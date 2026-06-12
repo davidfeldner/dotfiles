e775a0fee68f98fd0bf2629b41196b1cdf911c25
davidfeldner <davidfeldner12@gmail.com>
2026-06-12 03:35:26 +0200

    Fix after update

--- modules/home/hypr/hyprland.nix
+++ modules/home/hypr/hyprland.nix
@@ -74,7 +74,7 @@
 
         gtk = {
           enable = true;
-          gtk4.theme = config.gtk.theme;
+          # gtk4.theme = config.gtk.theme;
           gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
         };
 
